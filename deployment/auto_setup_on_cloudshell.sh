d2a06cf524f6b1796b6907b85f0e89807228da3a
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-03-03 12:22:32 +0900

    Enhanced automation of install script on CloudShell #65

--- deployment/auto_setup_on_cloudshell.sh
+++ deployment/auto_setup_on_cloudshell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# curl -s -o- https://raw.githubusercontent.com/aws-samples/siem-on-amazon-elasticsearch/master/deployment/auto_setup_on_cloudshell.sh | bash
+# bash <(curl -s -o- https://raw.githubusercontent.com/aws-samples/siem-on-amazon-elasticsearch/develop/deployment/auto_setup_on_cloudshell.sh)
 
 ###############################################################################
 # helper Function
@@ -12,30 +12,42 @@ function func_get_cdk_json () {
     --region $AWS_DEFAULT_REGION \
     --query "Parameter.Value" \
     --output text > cdk.json.ssm
-  if [ -f "cdk.json" ]; then
-    file_diff=`diff cdk.json.ssm cdk.json | wc -l`
-    if [ $file_diff == "0" ]; then
-      mv cdk.json.ssm cdk.json
-    else
-      echo "cdk.json on local and downloaded file from SSM parameter store are different."
-      while true; do
-        read -p 'Continue with local file? or exit? [Y(=continue)/n(=exit)]' Answer
-        case $Answer in
-          '' | [Yy]* )
-            echo Continue
-            break;
-            ;;
-          [Nn]* )
-            echo exit. bye
-            exit;
-            ;;
-          * )
-            echo Please answer YES or NO.
-        esac
-      done;
+  aws ssm get-parameter \
+    --name "/aes-siem/cdk/cdk.context.json" \
+    --region $AWS_DEFAULT_REGION \
+    --query "Parameter.Value" \
+    --output text > cdk.context.json.ssm
+  if [ ! -s "cdk.json.ssm" ]; then
+    rm cdk.json.ssm
+  fi
+  if [ ! -s "cdk.context.json.ssm" ]; then
+    rm cdk.context.json.ssm
+  fi
+  if [ -s "cdk.json" ]; then
+    if [ -f "cdk.json.ssm" ]; then
+      file_diff=`diff cdk.json.ssm cdk.json | wc -l`
+      if [ $file_diff != "0" ]; then
+        # differenct file
+        echo "cdk.json on local and downloaded file from SSM parameter store are different."
+        while true; do
+          read -p 'Continue with local file? or exit? [Y(=continue) / n(=exit)]' Answer
+          case $Answer in
+            '' | [Yy]* )
+              echo Continue
+              break;
+              ;;
+            [Nn]* )
+              echo exit. bye
+              exit;
+              ;;
+            * )
+              echo Please answer YES or NO.
+          esac
+        done;
+      fi
     fi
-  else
-    mv cdk.json.ssm cdk.json
+  elif [ -f "cdk.json.ssm" ]; then
+    cp cdk.json.ssm cdk.json
   fi
 }
 
@@ -67,10 +79,27 @@ function func_ask_and_set_env {
     --type String
 }
 
+function func_continue_or_exit () {
+  while true; do
+    read -p 'Do you ontinue or exit? [Y(=continue) / n(=exit)]' Answer
+    case $Answer in
+      [Yy]* )
+        echo Continue
+        break;
+        ;;
+      [Nn]* )
+        echo exit. bye
+        exit;
+        ;;
+      * )
+        echo Please answer YES or NO.
+    esac
+  done;
+}
+
 ###############################################################################
 # main script
 ###############################################################################
-
 
 # 1. Setting Up the AWS CDK Execution Environment
 cd ~/
@@ -79,9 +108,7 @@ sudo yum install -y amazon-linux-extras
 sudo amazon-linux-extras enable python3.8
 sudo yum install -y python38 python38-devel git jq
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-if [ -f /usr/bin/pip3 ]; then
-  :
-else
+if [ ! -f /usr/bin/pip3 ]; then
   sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.8 1
 fi
 
@@ -99,8 +126,6 @@ if [ -v ${CDK_DEFAULT_ACCOUNT} ]; then
   read -p "Enter CDK_DEFAULT_ACCOUNT: default is [$GUESS_CDK_DEFAULT_ACCOUNT]: " CDK_DEFAULT_ACCOUNT
   CDK_DEFAULT_ACCOUNT=${CDK_DEFAULT_ACCOUNT:-$GUESS_CDK_DEFAULT_ACCOUNT}
   export CDK_DEFAULT_ACCOUNT
-else
-  :
 fi
 
 EC2_AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e s/.$//`
@@ -108,15 +133,14 @@ GUESS_AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-$EC2_AWS_DEFAULT_REGION}
 read -p "Enter AWS_DEFAULT_REGION to deploy Amazon ES: default is [$GUESS_AWS_DEFAULT_REGION]: " AWS_DEFAULT_REGION
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-$GUESS_AWS_DEFAULT_REGION}
 export AWS_DEFAULT_REGION
+export CDK_DEFAULT_REGION=$AWS_DEFAULT_REGION
 echo "Your AWS account is $CDK_DEFAULT_ACCOUNT"
-echo "Installation target AWS region is $AWS_DEFAULT_REGION"
+echo "AWS region of installation target is $AWS_DEFAULT_REGION"
 
 # additional
 cd ~/siem-on-amazon-elasticsearch/source/cdk/
 func_get_cdk_json
-if [ -f "cdk.json" ]; then
-  :
-else
+if [ ! -f "cdk.json" ]; then
   func_ask_and_set_env
 fi
 
@@ -136,18 +160,83 @@ cdk bootstrap
 echo "################################################"
 echo "# Next Procedure"
 echo "################################################"
-echo "1. change directory to CDK dir"
-echo "cd siem-on-amazon-elasticsearch/source/cdk/"
-echo "2. Edit cdk.json."
-echo "3. enable cdk env."
-echo "source .env/bin/activate"
-echo "4. Execute 'cdk deploy'"
-echo "5. push cdk.json to SSM Parameter Store"
-cat << EOF
-aws ssm put-parameter \
-  --name "/aes-siem/cdk/cdk.json" \
-  --overwrite \
-  --region $AWS_DEFAULT_REGION \
-  --value "\$(cat cdk.json)" \
-  --type String
-EOF
+echo "Open ANOTHER shell and edit cdk.json"
+echo "If you just update SIEM, you don't need to edit cdk.json"
+echo ""
+echo '```'
+echo "cd ~/siem-on-amazon-elasticsearch/source/cdk/"
+echo "vi cdk.json"
+echo '# validation. 0 is OK'
+echo 'cat cdk.json.corrupt | jq empty; echo $?'
+echo '```'
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Did you edit cdk.json and continue?"
+func_continue_or_exit
+
+source .env/bin/activate
+cdk deploy
+
+if [ -f "cdk.json.ssm" ]; then
+  file_diff=`diff cdk.json.ssm cdk.json | wc -l`
+  if [ $file_diff != "0" ]; then
+    # it must be updated
+    aws ssm put-parameter \
+      --name "/aes-siem/cdk/cdk.json" \
+      --overwrite \
+      --region $AWS_DEFAULT_REGION \
+      --value "$(cat cdk.json)" \
+      --type String
+  fi
+  rm cdk.json.ssm
+else
+  aws ssm put-parameter \
+    --name "/aes-siem/cdk/cdk.json" \
+    --overwrite \
+    --region $AWS_DEFAULT_REGION \
+    --value "$(cat cdk.json)" \
+    --type String
+fi
+
+if [ -f "cdk.context.json.ssm" ]; then
+  file_diff=`diff cdk.context.json.ssm cdk.context.json | wc -l`
+  if [ $file_diff != "0" ]; then
+    # it must be updated
+    aws ssm put-parameter \
+      --name "/aes-siem/cdk/cdk.context.json" \
+      --overwrite \
+      --region $AWS_DEFAULT_REGION \
+      --value "$(cat cdk.context.json)" \
+      --type String
+  fi
+  rm cdk.context.json.ssm
+else
+  aws ssm put-parameter \
+    --name "/aes-siem/cdk/cdk.context.json" \
+    --overwrite \
+    --region $AWS_DEFAULT_REGION \
+    --value "$(cat cdk.context.json)" \
+    --type String
+fi
+
+echo "All script was done"
+
+
+#echo "1. change directory to CDK dir"
+#echo "cd siem-on-amazon-elasticsearch/source/cdk/"
+#echo "2. Edit cdk.json."
+#echo "3. enable cdk env."
+#echo "source .env/bin/activate"
+#echo "4. Execute CDK"
+#echo "cdk deploy"
+#echo "5. push cdk.json to SSM Parameter Store"
+#cat << EOF
+#aws ssm put-parameter \
+#  --name "/aes-siem/cdk/cdk.json" \
+#  --overwrite \
+#  --region $AWS_DEFAULT_REGION \
+#  --value "\$(cat cdk.json)" \
+#  --type String
+#EOF
