415bcc9062c4d3287b051a3ec67ab38ff07b93b9
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-03-13 18:45:54 +0900

    added network.transport and deleted destination.bytes of ECS field #71

--- source/lambda/es_loader/siem/sf_vpcflowlogs.py
+++ source/lambda/es_loader/siem/sf_vpcflowlogs.py
@@ -10,6 +10,25 @@ def transform(logdata):
     else:
         logdata['event']['outcome'] = 'unknown'
 
+    if logdata['protocol'] == "6":
+        logdata['network']['transport'] = 'tcp'
+    elif logdata['protocol'] == "17":
+        logdata['network']['transport'] = 'udp'
+    elif logdata['protocol'] == "1":
+        logdata['network']['transport'] = 'icmp'
+    elif logdata['protocol'] == "41":
+        logdata['network']['transport'] = 'ipv6'
+    elif logdata['protocol'] == "8":
+        logdata['network']['transport'] = 'egp'
+    elif logdata['protocol'] == "33":
+        logdata['network']['transport'] = 'dccp'
+    elif logdata['protocol'] == "42":
+        logdata['network']['transport'] = 'sdrp'
+    elif logdata['protocol'] == "47":
+        logdata['network']['transport'] = 'gre'
+    elif logdata['protocol'] == "132":
+        logdata['network']['transport'] = 'sctp'
+
     try:
         logdata['network']['type'] = logdata['network']['type'].lower()
     except KeyError:
