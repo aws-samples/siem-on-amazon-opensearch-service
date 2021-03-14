989f038fdc775666919b8fc8bb3afd76af4b9a15
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-03-14 21:44:50 +0900

    bumpup to v2.3.0-beta.3

--- source/lambda/deploy_es/index.py
+++ source/lambda/deploy_es/index.py
@@ -16,7 +16,7 @@ import requests
 from crhelper import CfnResource
 from requests_aws4auth import AWS4Auth
 
-__version__ = '2.3.0-beta.2'
+__version__ = '2.3.0-beta.3'
 print('version: ' + __version__)
 
 logger = logging.getLogger(__name__)
