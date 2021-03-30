b7248ec2dda96ca6f76ade3ac05ebf0991e89428
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-03-30 21:14:39 +0900

    bumpup to v2.3.1-beta.2

--- source/lambda/es_loader/index.py
+++ source/lambda/es_loader/index.py
@@ -16,7 +16,7 @@ from aws_lambda_powertools.metrics import MetricUnit
 import siem
 from siem import geodb, utils
 
-__version__ = '2.3.1-beta.1'
+__version__ = '2.3.1-beta.2'
 
 
 logger = Logger(stream=sys.stdout, log_record_order=["level", "message"])
--- source/lambda/geoip_downloader/index.py
+++ source/lambda/geoip_downloader/index.py
@@ -11,7 +11,7 @@ import urllib.request
 
 import boto3
 
-__version__ = '2.3.1-beta.1'
+__version__ = '2.3.1-beta.2'
 
 # get var from lambda environment
 try:
