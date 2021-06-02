4bcdff6e97a78319a4f5852b4f281d5eb0557644
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-06-02 21:05:59 +0900

    changed default repo dir and modified logic error for auto install script #65

--- deployment/auto_setup_on_cloudshell.sh
+++ deployment/auto_setup_on_cloudshell.sh
@@ -5,8 +5,24 @@
 ###############################################################################
 # helper Function
 ###############################################################################
+LOG_OUT="$HOME/auto_setup_on_cloudshell-$(date "+%Y%m%d_%H%M%S").log"
+exec 2> >(tee -a $LOG_OUT) 1>&2
 
-export BASEDIR="$HOME/siem-on-amazon-elasticsearch"
+export BASEDIR="$HOME/siem-on-amazon-elasticsearch-service"
+export OLDDIR="$HOME/siem-on-amazon-elasticsearch"
+
+function func_migrate_old_repo_to_new_repo () {
+  if [ -s "${OLDDIR}/source/cdk/cdk.json" ]; then
+    if [ ! -e "${BASEDIR}/source/cdk/cdk.json" ]; then
+      cp "${OLDDIR}/source/cdk/cdk.json" "${BASEDIR}/source/cdk/cdk.json"
+    fi
+  fi
+  if [ -s "${OLDDIR}/source/cdk/cdk.context.json" ]; then
+    if [ ! -e "${BASEDIR}/source/cdk/cdk.context.json" ]; then
+      cp "${OLDDIR}/source/cdk/cdk.context.json" "${BASEDIR}/source/cdk/cdk.context.json"
+    fi
+  fi
+}
 
 function func_put_to_ssm_param () {
   cd $BASEDIR/source/cdk
@@ -14,35 +30,35 @@ function func_put_to_ssm_param () {
   aws ssm put-parameter \
     --name "/aes-siem/cdk/$put_obj" \
     --overwrite \
-    --region $AWS_DEFAULT_REGION \
-    --value "$(cat $put_obj)" \
+    --region "$AWS_DEFAULT_REGION" \
+    --value "$(cat ${put_obj})" \
     --type String
   echo "PUT $put_obj to SSM Parameter store"
 }
 
-function func_get_from_param () {
+function func_get_from_param_store () {
   cd $BASEDIR/source/cdk
   get_obj=$1
   aws ssm get-parameter \
     --name "/aes-siem/cdk/$get_obj" \
-    --region $AWS_DEFAULT_REGION \
+    --region "$AWS_DEFAULT_REGION" \
     --query "Parameter.Value" \
-    --output text > ${get_obj}.ssm 2> /dev/null
+    --output text > "${get_obj}.ssm" 2> /dev/null
   if [ -s "${get_obj}.ssm" ]; then
     echo "GET $get_obj from SSM Parameter store"
   else
-    rm ${get_obj}.ssm
+    rm "${get_obj}.ssm"
   fi
 }
 
 function func_update_param () {
   cd $BASEDIR/source/cdk
   file_obj=$1
-  func_get_from_param $file_obj
+  func_get_from_param_store "${file_obj}"
   if [ ! -s "$file_obj.ssm" ]; then
     # not file in ssm
     if [ -s "$file_obj" ]; then
-      func_put_to_ssm_param $file_obj
+      func_put_to_ssm_param "${file_obj}"
     fi
   else
     if [ -s "$file_obj" ]; then
@@ -55,13 +71,13 @@ function func_update_param () {
   fi
 }
 
-function func_get_cdk_json () {
-  func_get_from_param cdk.json
-  func_get_from_param cdk.context.json
+function func_check_or_get_exiting_cdk_json () {
+  suffix=`date "+%Y%m%d_%H%M%S"`
+  func_get_from_param_store cdk.json
   if [ -s "cdk.json" ]; then
-    if [ -f "cdk.json.ssm" ]; then
+    if [ -s "cdk.json.ssm" ]; then
       file_diff=`diff cdk.json.ssm cdk.json | wc -l`
-      if [ $file_diff != "0" ]; then
+      if [ "$file_diff" != "0" ]; then
         # different file
         echo "cdk.json on local and downloaded cdk.json from SSM parameter store are different."
         echo ""
@@ -70,39 +86,40 @@ function func_get_cdk_json () {
         echo "Above lines are different"
         echo "  < exist only local cdk.json"
         echo "  > exist only downloaded cdk.json from SSM parameter store"
+        mv cdk.json "cdk.json-$suffix"
+        mv cdk.json.ssm cdk.json
+        echo "cdk.json in local is backupped to cdk.json-$suffix"
         echo ""
-        while true; do
-          read -p 'Overwrite parameter store with local file and Continue? or exit? [Y(=continue) / n(=exit)]: ' Answer
-          case $Answer in
-            '' | [Yy]* )
-              echo Continue
-              func_put_to_ssm_param "cdk.json"
-              rm cdk.json.ssm
-              if [ -s "cdk.context.json" ]; then
-                func_put_to_ssm_param "cdk.context.json"
-                rm cdk.context.json.ssm
-              fi
-              break;
-              ;;
-            [Nn]* )
-              echo exit. bye
-              exit;
-              ;;
-            * )
-              echo Please answer YES or NO.
-          esac
-        done;
       else
         # cdk.json and cdk.json.ssm are same file
         rm cdk.json.ssm
       fi
+    else
+      # There is no file in ssm paramater stores.
+      # This may be first time to execute this script
+      func_put_to_ssm_param cdk.json
     fi
   elif [ -f "cdk.json.ssm" ]; then
     # CDK was deployed and cdk.json is stored in ssm, but this is new environment
     mv cdk.json.ssm cdk.json
+  fi
+
+  func_get_from_param_store cdk.context.json
+  if [ -s "cdk.context.json" ]; then
     if [ -s "cdk.context.json.ssm" ]; then
-      mv cdk.context.json.ssm cdk.context.json
+      file_diff=`diff cdk.context.json.ssm cdk.context.json | wc -l`
+      if [ "$file_diff" != "0" ]; then
+        mv cdk.context.json "cdk.context.json-$suffix"
+        mv cdk.context.json.ssm cdk.context.json
+      else
+        # cdk.json and cdk.json.ssm are same file
+        rm cdk.context.json.ssm
+      fi
+    else
+      func_put_to_ssm_param cdk.context.json
     fi
+  elif [ -f "cdk.context.json.ssm" ]; then
+    mv cdk.context.json.ssm cdk.context.json
   fi
 }
 
@@ -130,12 +147,13 @@ function func_ask_and_set_env {
   done;
   aws ssm put-parameter \
     --name "/aes-siem/cdk/cdk.json" \
-    --region $AWS_DEFAULT_REGION \
+    --region "$AWS_DEFAULT_REGION" \
     --value "$(cat cdk.json)" \
     --type String
 }
 
 function func_validate_json () {
+  echo "func_validate_json"
   cd $BASEDIR/source/cdk
   file_obj=$1
   while true; do
@@ -147,9 +165,9 @@ function func_validate_json () {
         exit;
         ;;
       [Yy]* )
-        func_get_from_param cdk.json
-        cp cdk.json.ssm cdk.json
-        ERROR_MSG="$(cat $file_obj | jq empty 2>&1 > /dev/null)"
+        func_get_from_param_store cdk.json
+        cp -f cdk.json.ssm cdk.json
+        ERROR_MSG="$(cat ${file_obj} | jq empty 2>&1 > /dev/null)"
         RESULT="$?"
         case $RESULT in
           0 )
@@ -158,7 +176,7 @@ function func_validate_json () {
           * )
             echo "";
             echo "Woops. Your cdk.json is currupt json format";
-            echo $ERROR_MSG;
+            echo "$ERROR_MSG";
             ;;
         esac
         ;;
@@ -196,39 +214,46 @@ cd ~/
 
 echo 'yum groups mark install -y "Development Tools"'
 sudo yum groups mark install -y "Development Tools" > /dev/null
-
-
 echo -e "Done\n"
+
 echo "sudo yum install -y amazon-linux-extras"
 sudo yum install -y amazon-linux-extras > /dev/null
 echo -e "Done\n"
+
 echo "sudo amazon-linux-extras enable python3.8"
 sudo amazon-linux-extras enable python3.8 > /dev/null
 echo -e "Done\n"
+
 echo "yum install -y python38 python38-devel git jq"
 sudo yum install -y python38 python38-devel git jq > /dev/null
 echo -e "Done\n"
+
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 if [ ! -f /usr/bin/pip3 ]; then
   sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.8 1
 fi
 
-if [ -d "siem-on-amazon-elasticsearch" ]; then
-  echo "git rebase"
+#if [ -d "siem-on-amazon-elasticsearch" ]; then
+if [ -d "$BASEDIR" ]; then
+  echo "git rebase to get latest commit"
   cd $BASEDIR
+  git checkout main
   git pull --rebase > /dev/null
+  cd $HOME
 else
-  echo "git clone"
-  git clone https://github.com/aws-samples/siem-on-amazon-elasticsearch.git > /dev/null
+  echo "git clone siem source code"
+  git clone https://github.com/aws-samples/siem-on-amazon-elasticsearch-service.git > /dev/null
 fi
+cd $BASEDIR && echo `git log | head -1` && cd $HOME
 echo -e "Done\n"
 
 echo "### 2. Setting Environment Variables ###"
-
+### set AWS Accont ###
 GUESS_CDK_DEFAULT_ACCOUNT=`aws sts get-caller-identity --query 'Account' --output text`
 read -p "Enter CDK_DEFAULT_ACCOUNT: default is [$GUESS_CDK_DEFAULT_ACCOUNT]: " TEMP_CDK_DEFAULT_ACCOUNT
 export CDK_DEFAULT_ACCOUNT=${TEMP_CDK_DEFAULT_ACCOUNT:-$GUESS_CDK_DEFAULT_ACCOUNT}
 
+### set AWS Region ###
 if [[ $AWS_EXECUTION_ENV == "CloudShell" ]]; then
   GUESS_AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
   unset AWS_REGION
@@ -239,16 +264,21 @@ read -p "Enter AWS_DEFAULT_REGION to deploy Amazon ES: default is [$GUESS_AWS_DE
 export AWS_DEFAULT_REGION=${TEMP_AWS_DEFAULT_REGION:-$GUESS_AWS_DEFAULT_REGION}
 export CDK_DEFAULT_REGION=$AWS_DEFAULT_REGION
 
+### print env ###
 echo ""
 echo "Your AWS account is $CDK_DEFAULT_ACCOUNT"
 echo "AWS region of installation target is $CDK_DEFAULT_REGION"
 echo ""
 
-# additional
-echo "func_get_cdk_json"
-func_get_cdk_json
+### Check if cdk.json exists. If no, create new one ###
+echo "func_migrate_old_repo_to_new_repo"
+func_migrate_old_repo_to_new_repo
+echo "func_check_or_get_exiting_cdk_json"
+func_check_or_get_exiting_cdk_json
 echo -e "Done\n"
 if [ ! -f "cdk.json" ]; then
+  ### create new one ###
+  echo "There is no existing cdk.json"
   echo "func_ask_and_set_env"
   func_ask_and_set_env > /dev/null
   echo -e "Done\n"
@@ -257,13 +287,16 @@ fi
 echo "### 3. Creating an AWS Lambda Deployment Package ###"
 cd $BASEDIR/deployment/cdk-solution-helper/
 echo "./step1-build-lambda-pkg.sh"
+date
 chmod +x ./step1-build-lambda-pkg.sh && ./step1-build-lambda-pkg.sh > /dev/null
 echo -e "Done\n"
 
 echo "### 4. Setting Up the Environment for AWS Cloud Development Kit (AWS CDK) ###"
 echo "./step2-setup-cdk-env.sh"
+date
 chmod +x ./step2-setup-cdk-env.sh && ./step2-setup-cdk-env.sh> /dev/null
 source ~/.bash_profile
+nvm use lts/*
 echo -e "Done\n"
 
 echo "### 5. Setting Installation Options with the AWS CDK ###"
@@ -279,7 +312,7 @@ echo "################################################"
 echo "1. Go to Systems Manager / Parameter Store in selected region"
 echo "   https://console.aws.amazon.com/systems-manager/parameters/aes-siem/cdk/cdk.json/"
 echo "2. Check and Edit cdk.json file in Parameter Store"
-echo "   If you want to update SIEM without change, please just return "
+echo "   If you want to update SIEM without changes, please just return "
 echo "   see more details https://github.com/aws-samples/siem-on-amazon-elasticsearch/blob/main/docs/deployment.md"
 echo ""
 echo ""
@@ -297,7 +330,9 @@ func_continue_or_exit
 
 (sleep 120 && func_update_param cdk.context.json > /dev/null 2>&1 & )
 echo "cdk deploy"
+date
 cdk deploy
 
 func_update_param cdk.context.json
 echo "All script was done"
+date
--- deployment/cdk-solution-helper/step2-setup-cdk-env.sh
+++ deployment/cdk-solution-helper/step2-setup-cdk-env.sh
@@ -22,11 +22,12 @@ echo "python3 -m pip install boto3 --user"
 python3 -m pip install boto3 --user
 
 echo "Install Node.js"
-curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts node
 nvm alias default lts/*
 node -e "console.log('Running Node.js ' + process.version)"
+nvm use lts/*
 echo "Install CDK"
 npm install -g aws-cdk
 
