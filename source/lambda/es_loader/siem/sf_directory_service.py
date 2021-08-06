0f19c479f28614bdef8997b4ccbfc7cce6f9fca2
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-08-06 23:07:56 +0900

    enhanced ECS mapping for Managed AD #118

--- source/lambda/es_loader/siem/sf_directory_service.py
+++ source/lambda/es_loader/siem/sf_directory_service.py
@@ -1,5 +1,15 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+from siem import utils
+from siem.winevtxml import initial_extract_action_outcome
+from siem.sf_windows_event import extract_instance_id
+
+
 def transform(logdata):
+    win_dict = initial_extract_action_outcome(logdata)
+    win_dict = extract_instance_id(logdata, win_dict)
+
+    logdata = utils.merge_dicts(logdata, win_dict)
+
     return logdata
