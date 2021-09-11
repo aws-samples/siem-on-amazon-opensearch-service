1a3f043a94339ad2cf4286f9953d75afb062320b
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-09-11 11:50:03 +0900

    modified cfn deployment macro for Amazon OpenSearch Service #157

--- source/cdk/app.py
+++ source/cdk/app.py
@@ -8,8 +8,11 @@ from aws_cdk import core
 
 from mysiem.aes_siem_stack import MyAesSiemStack
 
+__version__ = '2.5.0-beta.1'
+
 app = core.App()
-MyAesSiemStack(app, "aes-siem", description='SIEM on Amazon ES',
+MyAesSiemStack(app, "aes-siem",
+               description=f'SIEM on Amazon OpenSearch Service v{__version__}',
                env=core.Environment(
                     account=os.environ['CDK_DEFAULT_ACCOUNT'],
                     region=os.environ['CDK_DEFAULT_REGION']))
