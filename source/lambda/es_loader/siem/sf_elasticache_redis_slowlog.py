61c1a776019cf8a9520cf5d245ac0738b3736ff9
Akihiro Nakajima <nakajiak@amazon.co.jp>
2021-11-13 10:55:39 +0900

    bumpup to v2.5.1-beta.3

--- source/lambda/es_loader/siem/sf_elasticache_redis_slowlog.py
+++ source/lambda/es_loader/siem/sf_elasticache_redis_slowlog.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT-0
 __copyright__ = ('Copyright Amazon.com, Inc. or its affiliates. '
                  'All Rights Reserved.')
-__version__ = '2.5.1-beta.2'
+__version__ = '2.5.1-beta.3'
 __license__ = 'MIT-0'
 __author__ = 'Akihiro Nakajima'
 __url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
@@ -10,14 +10,13 @@ __url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
 
 def transform(logdata):
     if logdata.get('ClientAddress'):
-        logdata['ClientAddress'], port = logdata[
-            'ClientAddress'].rsplit(':', 1)
+        ip, port = logdata['ClientAddress'].rsplit(':', 1)
         logdata['source'] = {}
-        logdata['source']['ip'] = logdata['ClientAddress']
+        logdata['source']['ip'] = ip
         logdata['source']['port'] = port
+        logdata['source']['address'] = ip
         if 'related' not in logdata:
             logdata['related'] = {}
-        logdata['source']['address'] = logdata['ClientAddress']
-        logdata['related']['ip'] = [logdata['ClientAddress'], ]
+        logdata['related']['ip'] = [ip, ]
 
     return logdata
