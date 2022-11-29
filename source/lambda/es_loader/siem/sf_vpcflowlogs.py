d8a74ed98158bae3139416ff882954a82ac2d6aa
Akihiro Nakajima <nakajiak@amazon.co.jp>
2022-11-29 20:05:35 +0900

    updated sf_vpcflowlogs.py for transit gateway #295

--- source/lambda/es_loader/siem/sf_vpcflowlogs.py
+++ source/lambda/es_loader/siem/sf_vpcflowlogs.py
@@ -9,13 +9,17 @@ __url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
 
 
 def transform(logdata):
-    action = logdata['event']['action']
-    if 'ACCEPT' in action:
-        logdata['event']['outcome'] = 'success'
-    elif 'REJECT' in action:
-        logdata['event']['outcome'] = 'failure'
+    is_transitgateway = logdata.get('resource_type')
+    if is_transitgateway:
+        logdata['@log_type'] = 'transit-gateway'
     else:
-        logdata['event']['outcome'] = 'unknown'
+        action = logdata['event'].get('action', '')
+        if 'ACCEPT' in action:
+            logdata['event']['outcome'] = 'success'
+        elif 'REJECT' in action:
+            logdata['event']['outcome'] = 'failure'
+        else:
+            logdata['event']['outcome'] = 'unknown'
 
     protocol = logdata.get('protocol')
     if protocol == "6":
