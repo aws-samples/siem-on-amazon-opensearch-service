a760d3c75040016e71131d2e617a96778921ef23
Akihiro Nakajima <nakajiak@amazon.co.jp>
2022-12-01 19:16:19 +0900

    Change cpu architecture of lambda function in 12 regions #336

--- source/cdk/mysiem/aes_siem_stack.py
+++ source/cdk/mysiem/aes_siem_stack.py
@@ -215,10 +215,15 @@ class MyAesSiemStack(cdk.Stack):
             arm = aws_lambda.Architecture.ARM_64.name
             x86 = aws_lambda.Architecture.X86_64.name
             # Lambda Arch
-            if region in ('ap-northeast-1',
-                          'ap-south-1', 'ap-southeast-1', 'ap-southeast-2',
-                          'eu-central-1', 'eu-west-1', 'eu-west-2',
-                          'us-east-1', 'us-east-2', 'us-west-2'):
+            if region in ('af-south-1', 'ap-east-1',
+                          'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
+                          'ap-south-1',
+                          'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3',
+                          'ca-central-1',
+                          'eu-central-1', 'eu-north-1', 'eu-south-1',
+                          'eu-west-1', 'eu-west-2', 'eu-west-3',
+                          'me-south-1', 'sa-east-1',
+                          'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'):
                 region_dict[region]['LambdaArch'] = arm
             else:
                 region_dict[region]['LambdaArch'] = x86
