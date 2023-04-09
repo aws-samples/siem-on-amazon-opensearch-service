e333175018a21dffb7ef7bcd380e39dd8c78086c
Akihiro Nakajima <nakajiak@amazon.co.jp>
2023-04-09 14:38:43 +0900

    bumpup to v2.9.1-rc.1

--- source/lambda/geoip_downloader/index.py
+++ source/lambda/geoip_downloader/index.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT-0
 __copyright__ = ('Copyright Amazon.com, Inc. or its affiliates. '
                  'All Rights Reserved.')
-__version__ = '2.9.1-beta.2'
+__version__ = '2.9.1-rc.1'
 __license__ = 'MIT-0'
 __author__ = 'Akihiro Nakajima'
 __url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
