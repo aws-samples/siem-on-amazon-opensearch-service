6806ef7e42ad89a05e8b49210735579e885e320f
Akihiro Nakajima <nakajiak@amazon.co.jp>
2023-07-16 23:19:27 +0900

    added sf_ocsf_json.py for third-party ocsf #374

---
+++ source/lambda/es_loader/siem/sf_ocsf_json.py
@@ -0,0 +1,15 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+__copyright__ = ('Copyright Amazon.com, Inc. or its affiliates. '
+                 'All Rights Reserved.')
+__version__ = '2.10.0-beta.3'
+__license__ = 'MIT-0'
+__author__ = 'Akihiro Nakajima'
+__url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
+
+from siem import sf_securitylake
+
+
+def transform(logdata):
+    logdata = sf_securitylake.transform(logdata)
+    return logdata
