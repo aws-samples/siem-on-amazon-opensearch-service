8893d281aa336566975c166875ba2d36c34ca911
Akihiro Nakajima <nakajiak@amazon.co.jp>
2023-09-06 12:40:43 +0900

    configure to use Arm64 architecture of lambda function in 6 additional regions #400

--- source/cdk/mysiem/aes_siem_stack.py
+++ source/cdk/mysiem/aes_siem_stack.py
@@ -247,12 +247,15 @@ class MyAesSiemStack(cdk.Stack):
             # Lambda Arch
             if region in ('af-south-1', 'ap-east-1',
                           'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
-                          'ap-south-1',
+                          'ap-south-1', 'ap-south-2',
                           'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3',
+                          'ap-southeast-4',
                           'ca-central-1',
-                          'eu-central-1', 'eu-north-1', 'eu-south-1',
+                          'eu-central-1', 'eu-north-1', 'eu-central-2',
+                          'eu-south-1', 'eu-south-2',
                           'eu-west-1', 'eu-west-2', 'eu-west-3',
-                          'me-south-1', 'sa-east-1',
+                          'il-central-1',
+                          'me-central-1', 'me-south-1', 'sa-east-1',
                           'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'):
                 region_dict[region]['LambdaArch'] = arm
             else:
@@ -597,12 +600,13 @@ class MyAesSiemStack(cdk.Stack):
             self, "HasLambdaArchitecturesProp",
             expression=cdk.Fn.condition_not(
                 cdk.Fn.condition_or(
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'ap-south-2'),
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'ap-southeast-4'),
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'eu-central-2'),
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'eu-south-2'),
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'il-central-1'),
-                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'me-central-1'),
+                    cdk.Fn.condition_equals(cdk.Aws.REGION, 'ap-dummy-99'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'ap-south-2'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'ap-southeast-4'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'eu-central-2'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'eu-south-2'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'il-central-1'),
+                    #cdk.Fn.condition_equals(cdk.Aws.REGION, 'me-central-1'),
                 )
             )
         )
