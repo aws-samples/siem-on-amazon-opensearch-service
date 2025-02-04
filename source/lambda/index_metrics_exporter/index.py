03e2d903b048168a508157f79a0dec2bd6f083dd
Akihiro Nakajima <nakajiak@amazon.co.jp>
2025-02-04 23:11:38 +0900

    bumpup to v2.10.4-beta.1

--- source/lambda/index_metrics_exporter/index.py
+++ source/lambda/index_metrics_exporter/index.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT-0
 __copyright__ = ('Copyright Amazon.com, Inc. or its affiliates. '
                  'All Rights Reserved.')
-__version__ = '2.10.3'
+__version__ = '2.10.4-beta.1'
 __license__ = 'MIT-0'
 __author__ = 'Akihiro Nakajima'
 __url__ = 'https://github.com/aws-samples/siem-on-amazon-opensearch-service'
